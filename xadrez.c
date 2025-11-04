6c43bd6c0a3f93503581cfe36d9bff12addb0ace
André Sousa <andre_sbatista@outlook.com>
2025-11-04 02:04:39 +0000

    Commit final do desafio mestre do xadrez, implementei as funções solicitadas no trabalho com loops aninhados e recursividade para o bispo e para o cavalo.

--- xadrez.c
+++ xadrez.c
@@ -1,56 +1,85 @@
 #include <stdio.h>
+/*
+Em minhas pesquisar encontrei as VARIAVEIS GLOBAIS, queria resolver o problema com a recursividade.
+Ná saida de dados eu queria contar a quantidade de passos como eu já havia feito no trabalho anterior de forma crescente.
+e não seria possivel utilizando a variavel movimento pos eu drecremento ela.
+*/
+int acumula_movimento;
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void movimentoRainha(int movimento) {
+    if (movimento > 0) {
+        acumula_movimento ++;
+        printf("A rainha andou %i casas para a ESQUERDA\n", acumula_movimento);
+        movimentoRainha(movimento - 1);
+    }
+    acumula_movimento = 0; // zero a variavel para ser utilizada  novamente.
+}
 
-int main()
+
+void movimentoTorre(int movimento){
+    if (movimento > 0) {
+        acumula_movimento++;
+        printf("A torre andou %i casas para DIREITA\n", acumula_movimento); 
+        movimentoTorre(movimento - 1);
+    }
+    acumula_movimento = 0; // zero a variavel para ser utilizada  novamente.
+}
+
+
+void movimentoCavalo(int movimento_frente, int movimento_lado)
 {
+    for (int i = 0; i < movimento_lado; i++)
+    {
+        for(int i = 0; i < movimento_frente; i++)
+        {
+            acumula_movimento++;
+            printf("O cavalo andou %d casas para CIMA\n",acumula_movimento);
+        }
+        acumula_movimento = movimento_lado; // subscrevi a variavel para o valor lado se não ela ia aparecer 2 casas como no movimento para cima
+        printf("O cavalo andou %d casas para a DIREITA\n",acumula_movimento);
+    }
+    acumula_movimento = 0; // zeroo a variavel para ser utilizada novamente;
+}
 
-    // Variaveis das peçãs.
+void movimentoBispo(int movimento)
+{
+    if (movimento > 0)
+    {
+        acumula_movimento++;
+        do
+        {
+            do
+            {
+                // movimento na horizontal interno
+                printf("O bispo andou %d na HORIZONTAL \n", acumula_movimento);
+            } while (movimento == 0);
+            // movimento na vertical externo
+            printf("O bispo andou %d na VERTICAL \n", acumula_movimento);
+        } while (movimento == 0);
+        movimentoBispo(movimento - 1);
+        /* code */
+    }
+    acumula_movimento = 0;// zera a variavel para ser utilizada novamente;
+}
+
+int main(){
     int casas_rainha = 8;
     int casas_bispo = 5;
     int casas_torre = 5;
-    int casas_reto_cavalo = 2;
-    int casas_lado_cavalo = 1;
-    int loop; // Criei a variavel loop para evitar criar mais de uma variavel com letras e fui subscrevendo de acordo com o que eu precisava
-
+    int casas_reto_cavalo = 2;         
+    int casas_lado_cavalo = 1;    
+    
     printf(" ----Movimentação da rainha---- \n");
-    // Dentro do for subscrevi o valor de loop para 1 dentro do for assim o 0 não conta.
-    for (loop = 1; loop <= casas_rainha; loop++)
-    {
-        printf("A rainha andou %i casas para a ESQUERDA\n", loop);
-    }
+    movimentoRainha(casas_rainha);
 
     printf(" ----Movimentação do bispo---- \n");
-    // Segui subscrevendo o valor de loop de acordo com o que precisava.
-    loop = 0;
-    do
-    {
-        loop++;
-        printf("O bispo andou %i casas para CIMA a DIREITA\n", loop);
-    } while (loop < casas_bispo);
+    movimentoBispo(casas_bispo);
 
     printf(" ----Movimentação da torre---- \n");
-    // Segui subscrevendo o valor de loop de acordo com o que precisava.
-    loop = 1;
-    while (loop <= casas_torre)
-    {
-        printf("A torre andou %i casas para FRENTE\n", loop);
-        loop++;
-    }
+    movimentoTorre(casas_torre);
 
-    // usei o do-wile para conseguir usar o for antes de printar uma vez a casa a direita do cavalo.
-    printf(" ----Movimentação do CAVALO---- \n");
-    do
-    {
-        for (loop = 1; loop <= casas_reto_cavalo; loop++)
-        {
-            printf("O cavalo andou %d casas para CIMA\n", loop);
-        }
-        printf("O cavalo andou %d casa para DIREITA", casas_lado_cavalo);
-        casas_lado_cavalo++;
-    } while (casas_lado_cavalo == 1);
+    printf(" ----Movimentação da torre---- \n");
+    movimentoCavalo(casas_reto_cavalo,casas_lado_cavalo);
 
     return 0;
 }
